32068f90394f74361136d7bf8b7b9c7f8957f369
Ankita Negi <ankitanegi1696@gmail.com>
2022-11-24 00:27:34 +0530

    adding parcel

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading2 = React.createElement("h2",{},"Hello React");
 const root2 = ReactDOM.createRoot(document.getElementById("root2"));
 root2.render(heading2)
+
+console.log("Hi");
